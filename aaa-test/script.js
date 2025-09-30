c4d2780f6429928e874a45f06ea5f86a68162e7d
clrkk25 <2220995109@qq.com>
2025-09-30 18:16:35 +0800

    Update size

--- aaa-test/script.js
+++ aaa-test/script.js
@@ -1,28 +1,7 @@
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-function golfScore(par,strokes){
+function reverseString(string){
   let arr="";
-  if(par===1)
-  {
-    arr="Hole-in-one!";
+    for(let i=0;i<string.length;i++){
+    arr+=string[string.length-1-i];
   }
-  else if(strokes<=par-2){
-    arr="Eagle";
-  }
-  else if(strokes===par-1){
-    arr="Birdie";
-  }
-  else if(strokes===par){
-    arr="Par";
-  }
-  else if(strokes===par+1){
-    arr="Bogey";
-  }
-  else if(strokes===par+2){ 
-    arr="Double Bogey";
-  }
-  else{
-    arr="Go Home!";
-  }
-
   return arr;
 }

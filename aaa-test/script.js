ba5f7cc0b052d68800ec7cd54b547f03c544c573
clrkk25 <2220995109@qq.com>
2025-09-30 18:11:38 +0800

    Update size

--- aaa-test/script.js
+++ aaa-test/script.js
@@ -1,74 +1,28 @@
-let lunches = [];
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par,strokes){
+  let arr="";
+  if(par===1)
+  {
+    arr="Hole-in-one!";
+  }
+  else if(strokes<=par-2){
+    arr="Eagle";
+  }
+  else if(strokes===par-1){
+    arr="Birdie";
+  }
+  else if(strokes===par){
+    arr="Par";
+  }
+  else if(strokes===par+1){
+    arr="Bogey";
+  }
+  else if(strokes===par+2){ 
+    arr="Double Bogey";
+  }
+  else{
+    arr="Go Home!";
+  }
 
-function addLunchToEnd(arr, string) {
-    arr.push(string);
-    console.log(`${string} added to the end of the lunch menu.`);
-    return arr;
+  return arr;
 }
-
-function addLunchToStart(arr, string) {
-    arr.unshift(string);
-    console.log(`${string} added to the start of the lunch menu.`);
-    return arr;
-}
-
-function removeLastLunch(arr) {
-    if(arr==""){
-        console.log("No lunches to remove.")
-    }
-    else{
-        let string=arr[arr.length-1];
-        arr.pop();
-        console.log(`${string} removed from the end of the lunch menu.`);
-    
-    }
-    return arr;
-    
-}
-
-function removeFirstLunch(arr) {
-    if(arr==""){
-        console.log("No lunches to remove.")
-    }
-    else{
-        let string=arr[0];
-        arr.shift();
-        console.log(`${string} removed from the start of the lunch menu.`);
-    
-    }
-    return arr;
-    
-}
-
-function getRandomLunch(arr) {
-    if(arr==""){
-        console.log("No lunches available.")
-    }
-    else{
-        let string=arr[Math.floor(Math.random() * arr.length)];
-        console.log(`Randomly selected lunch: ${string}`);
-    
-    }
-    
-}
-
-function showLunchMenu(arr) {
-    if(arr==""){
-        console.log("The menu is empty.")
-    }
-    else{
-        let string="";
-        for(let i=0;i<arr.length;i++){
-            string+=arr[i];
-            if(i<arr.length-1){
-                string+=", ";
-            }
-        }
-
-        console.log(`Menu items: ${string}`);
-    
-    }
-    
-}
-
-
